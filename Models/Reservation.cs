b2a05ac278d3127cbcd409273130006397a99ccb
Nicolas Lazarte <1853973@crosemont.qc.ca>
2023-11-02 00:24:02 -0400

    Insertion et affichage des données de la BD dans les pages Web

---
+++ Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projet_Final.Models
+{
+    public class Reservation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int VoitureId { get; set; }
+        public Voiture Voiture { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+    }
+}
