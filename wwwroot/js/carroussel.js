a5e96f1cba4cb87ada949509fe1d75a66147bf41
antoi <1682633@crosemont.qc.ca>
2023-11-27 04:38:37 +0800

    merge main

---
+++ wwwroot/js/carroussel.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    const sliderMe = () => {
+        let currentPosition = 0,
+            sliderItem = document.querySelectorAll('.slider-item'),
+            sliderItemWidth = window
+                .getComputedStyle(sliderItem[0])
+                .flexBasis.match(/\d+\.?\d+/g),
+            sliderInner = document.querySelector('.slider-inner');
+
+        const control = {
+            next: document.querySelector('#next'),
+            slideNext() {
+                currentPosition += parseFloat(sliderItemWidth);
+                if (currentPosition > limitPosition) {
+                    currentPosition = 0;
+                }
+                sliderInner.style.right = currentPosition + '%';
+            },
+            prev: document.querySelector('#prev'),
+            slidePrev() {
+                currentPosition -= parseFloat(sliderItemWidth);
+                if (currentPosition < 0) {
+                    currentPosition = limitPosition;
+                }
+                sliderInner.style.right = currentPosition + '%';
+            }
+        };
+
+        const limitPosition = sliderItemWidth * (sliderItem.length - Math.floor(100 / sliderItemWidth));
+
+        control.next.addEventListener('click', control.slideNext);
+        control.prev.addEventListener('click', control.slidePrev);
+
+        window.addEventListener("resize", function () {
+            currentPosition = 0;
+            document.querySelector('.slider-inner').style.right = currentPosition + '%';
+        });
+    };
+
+    sliderMe();
+
+    window.addEventListener("resize", sliderMe);
+});
---
+++ wwwroot/js/services.js
@@ -0,0 +1,5 @@
+﻿
+let card = document.querySelectorAll(".card");
+for (let i = 1; i <= card.length; i++) {
+	document.documentElement.style.setProperty("--face_height_" + i + "", document.querySelector(".card-container .card:nth-child(" + i + ") .face-2").scrollHeight + "px");
+}			
