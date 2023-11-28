64eeb65e047085897fd23ecc27701b2be5fbb000
Nicolas Lazarte <nicolaslv0018@gmail.com>
2023-11-27 21:24:01 -0500

    Notifier l'utilisateur quand il change son mot de passe et création d'un compte Admin

--- Areas/Identity/Pages/Account/Lockout.cshtml.cs
+++ Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -1,25 +1,98 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Projet_Final.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Projet_Final.Areas.Identity.Pages.Account
 {
-    /// <summary>
-    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-    ///     directly from your code. This API may change or be removed in future releases.
-    /// </summary>
+
     [AllowAnonymous]
     public class LockoutModel : PageModel
     {
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUtilisateur> _userManager;
+        private readonly ILogger<LockoutModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public LockoutModel(IConfiguration configuration, UserManager<ApplicationUtilisateur> userManager, ILogger<LockoutModel> logger, IEmailSender emailSender)
         {
+            _configuration = configuration;
+            _userManager = userManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null && _userManager.SupportsUserLockout && await _userManager.IsLockedOutAsync(user))
+            {
+                try
+                {
+                    var sendGridSettings = _configuration.GetSection("SendGridSettings");
+                    var apiKey = sendGridSettings["ApiKey"];
+                    var senderEmail = sendGridSettings["FromEmail"];
+                    var senderName = sendGridSettings["EmailName"];
+
+                    // Envoi d'un courriel via SendGrid
+                    var client = new SendGridClient(apiKey);
+                    var msg = new SendGridMessage()
+                    {
+                        From = new EmailAddress(senderEmail, senderName),
+                        Subject = "Notification de verrouillage de compte",
+                        PlainTextContent = "Votre compte a été verrouillé pour des mesures de sécurité, veuillez contacter l'administrateur."
+                    };
+                    msg.AddTo(new EmailAddress(user.Email));
+
+                    var emailResponse = await client.SendEmailAsync(msg);
+
+                    // Envoi d'un SMS via Twilio
+                    var twilioSettings = _configuration.GetSection("TwilioSettings");
+                    var accountSid = twilioSettings["AccountSId"];
+                    var authToken = twilioSettings["AuthToken"];
+                    var fromPhoneNumber = twilioSettings["FromPhoneNumber"];
+
+                    TwilioClient.Init(accountSid, authToken);
+
+                    var message = await MessageResource.CreateAsync(
+                        body: "Votre compte a été verrouillé pour des mesures de sécurité, veuillez contacter l'administrateur.",
+                        from: new Twilio.Types.PhoneNumber(fromPhoneNumber),
+                        to: new Twilio.Types.PhoneNumber("+" + user.PhoneNumber)
+                    );
+
+                    if (emailResponse.StatusCode == System.Net.HttpStatusCode.Accepted && message != null)
+                    {
+                        // Le courriel et le SMS ont été envoyés avec succès
+                        return RedirectToPage("/Index");
+                    }
+                    else
+                    {
+                        // Gérer l'échec de l'envoi du courriel ou du SMS
+                        _logger.LogError($"Error sending email. Status code: {emailResponse.StatusCode}");
+                        _logger.LogError($"SendGrid Response: {await emailResponse.Body.ReadAsStringAsync()}");
+                        _logger.LogError($"Error sending SMS. Status code: {message?.Status}");
+                        return Page();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Failed to send email or SMS. Error: {ex.Message}");
+                    return Page();
+                }
+            }
+
+            return Page();
         }
     }
 }
