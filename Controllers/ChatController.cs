58258325b19ca010eb32e5c10623c77058478fa8
antoi <1682633@crosemont.qc.ca>
2023-12-13 00:16:58 +0800

    ajout chat

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_Final.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("SupportChat");
+        }
+    }
+}
