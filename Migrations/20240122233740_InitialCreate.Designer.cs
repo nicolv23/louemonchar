e11299a07f54d5cf6546e1f12333cde1a0d253c7
Nicolas Lazarte <1853973@crosemont.qc.ca>
2024-01-22 18:48:20 -0500

    Arrangement de la page ListeModeles pour afficher toutes les images

--- Migrations/20240122233740_InitialCreate.Designer.cs
+++ Migrations/20240122233740_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projet_Final.Areas.Identity.Data;
 
@@ -11,9 +12,11 @@ using Projet_Final.Areas.Identity.Data;
 namespace Projet_Final.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240122233740_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
